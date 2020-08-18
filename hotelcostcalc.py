87ae0a58aa56a64904f7fcfff35be2c6923ede3e
Devansh Solani <53579366+devanshsolani@users.noreply.github.com>
2020-08-18 20:37:52 +0530

    Update hotelcostcalc.py

--- hotelcostcalc.py
+++ hotelcostcalc.py
@@ -1,5 +1,4 @@
 class hotelfarecal:
-
     def __init__(self,rt='',s=0,p=0,r=0,t=0,a=1800,name='',address='',cindate='',coutdate='',rno=101):
         print ("\n\n*****WELCOME TO DESOL HOTEL*****\n")
         self.rt=rt
@@ -73,26 +72,19 @@ class hotelfarecal:
         print ("1.Shorts----->Rs3","2.Trousers----->Rs4","3.Shirt--->Rs5","4.Jeans---->Rs6","5.Girlsuit--->Rs8","6.Exit")
 
         while (1):
-            #brought to you by code-projects.org
-
             e=int(input("Enter your choice:"))
-
             if (e==1):
                 f=int(input("Enter the quantity:"))
                 self.t=self.t+3*f
-
             elif (e==2):
                 f=int(input("Enter the quantity:"))
                 self.t=self.t+4*f
-
             elif (e==3):
                 f=int(input("Enter the quantity:"))
                 self.t=self.t+5*f
-
             elif (e==4):
                 f=int(input("Enter the quantity:"))
                 self.t=self.t+6*f
-
             elif (e==5):
                 f=int(input("Enter the quantity:"))
                 self.t=self.t+8*f
@@ -103,7 +95,6 @@ class hotelfarecal:
         print ("Total Laundary Cost=Rs",self.t,"\n")
     def gamebill(self):
         print ("******GAME MENU*******")
-
         print ("1.Table tennis----->Rs60","2.Bowling----->Rs80","3.Snooker--->Rs70","4.Video games---->Rs90","5.Pool--->Rs50==6","6.Exit")
         while (1):
             g=int(input("Enter your choice:"))
@@ -144,3 +135,29 @@ class hotelfarecal:
         print ("Additional Service Charges is",self.a)
         print ("Your grandtotal bill is:",self.rt+self.a,"\n")
         self.rno+=1
+def main():
+    a=hotelfarecal()
+    while (1):
+        print("1.Enter Customer Data")
+        print("2.Calculate rommrent")
+        print("3.Calculate restaurant bill")
+        print("4.Calculate laundry bill")
+        print("5.Calculate gamebill")
+        print("6.Show total cost")
+        print("7.EXIT")
+        b=int(input("\nEnter your choice:"))
+        if (b==1):
+            a.inputdata()
+        if (b==2):
+            a.roomrent()
+        if (b==3):
+            a.restaurentbill()
+        if (b==4):
+            a.laundrybill()
+        if (b==5):
+            a.gamebill()
+        if (b==6):
+            a.display()
+        if (b==7):
+            quit()
+main()
